5df0f590b733ade2303223e1d8111a0838e2351f
Dmitry Osipov <ibakepunk@gmail.com>
2017-07-20 00:46:33 +0300

    Slightly refactor func according to PEP8 and other linters

--- dict_differ.py
+++ dict_differ.py
@@ -1,7 +1,10 @@
 __author__ = 'ibakepunk'
+__email__ = 'ibakepunk@gmail.com'
+
+LIST_TYPES = (list, tuple)
 
 
-def get_diff(expected, actual, key=None):
+def get_diff(expected, actual, key: str = '') -> str:
     """
     Recursively gets xpath of elements that differ.
     :param expected: Expected dict
@@ -13,24 +16,33 @@ def get_diff(expected, actual, key=None):
     """
     diff = ''
     key = key or '/'
+
     if isinstance(expected, dict) and isinstance(actual, dict):
         d1_not_d2 = [k for k in expected if k not in actual]
         d2_not_d1 = [k for k in actual if k not in expected]
-        diff += 'Expected keys at {key} not found: {keys}'.format(key=key, keys=str(d1_not_d2) + '\n') if d1_not_d2 \
-            else ''
-        diff += 'Not expected keys at {key} found: {keys}'.format(key=key, keys=str(d2_not_d1) + '\n') if d2_not_d1 \
-            else ''
+
+        if d1_not_d2:
+            diff += 'Expected keys at {key} not found: {keys}'.format(
+                key=key, keys=str(d1_not_d2) + '\n')
+        if d2_not_d1:
+            diff += 'Not expected keys at {key} found: {keys}'.format(
+                key=key, keys=str(d2_not_d1) + '\n')
+
         for k in expected:
             if k in actual:
                 diff += get_diff(expected[k], actual[k], key + '/' + k)
-    elif isinstance(expected, list) and isinstance(actual, list):
+
+    elif isinstance(expected, LIST_TYPES) and isinstance(actual, LIST_TYPES):
         if len(expected) != len(actual):
-            diff += '{key} expected length {exp_len}, got {act_len}\n'.format(key=key, exp_len=len(expected),
-                                                                              act_len=len(actual))
+            diff += '{key} expected length {exp_len}, got {act_len}\n'.format(
+                key=key, exp_len=len(expected), act_len=len(actual))
             return diff
-        for i, (v1, v2) in enumerate(zip(expected, actual)):
-            diff += get_diff(v1, v2, key + '[' + str(i) + ']')
+        for i, (expected_value, actual_value) \
+                in enumerate(zip(expected, actual)):
+            diff += get_diff(expected_value, actual_value,
+                             key + '[' + str(i) + ']')
     else:
         if expected != actual:
-            diff += '{key} must be {exp}, got {act}\n'.format(key=key, exp=str(expected), act=str(actual))
+            diff += '{key} must be {exp}, got {act}\n'.format(
+                key=key, exp=str(expected), act=str(actual))
     return diff
